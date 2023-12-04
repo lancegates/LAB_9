bad36c57c14f35e3ad02b25f347377f21bafc4ad
Branden Hoskins <bh596@DIGITAL-37.ee.byu.edu>
2023-12-04 15:22:01 -0700

    adding files

---
+++ lab9_project/ball.h
@@ -0,0 +1,15 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#define BALL_RADIUS 10
+
+void ball_init();
+
+void ball_tick();
+
+bool ball_hit_ground();
+
+//add get location
+
+//maybe  speed and angle as variable in here if we want to change it from other locations
+
