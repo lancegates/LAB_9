91404648e74aca6fb2a225805cbb0e7397e2fd33
Branden Hoskins <bh596@DIGITAL-37.ee.byu.edu>
2023-12-04 16:22:57 -0700

    created main and added to game control and ball

--- lab9_project/ball.c
+++ lab9_project/ball.c
@@ -4,6 +4,14 @@
 #include <stdio.h>
 
 #define BALL_BASE_SPEED 30
+#define BALL_COLOR DISPLAY_WHITE
+#define BACKGROUND_COLOR DISPLAY_BLACK
+
+#define DEG_90 90
+#define DEG_180 190
+
+void drawBall();
+void eraseBall();
 
 // Used to track the current x,y
 uint16_t x_current;
@@ -19,9 +27,46 @@ uint16_t angle;
 bool dead;
 
 // Make State Machine
+typedef enum {
+  init_st,      // Start here, transition out of this state on the first tick.
+  on_paddle_st, // Go here when ball needs to be respawned(whiole it is sitting
+                // on the paddle)
+  moving_st,    // In this state while it's moving
+  dead_st, // here if the ball is dead (after it hits the ground or you won)
+} plane_st_t;
 
-void ball_init() {}
+plane_st_t currentState;
 
-void ball_tick() {}
+void ball_init() { currentState = init_st; }
+
+void ball_tick() {
+  switch (currentState) {
+  case init_st:
+    currentState = on_paddle_st;
+    dead = false;
+    angle = DEG_90;
+    break;
+
+  case on_paddle_st:
+    /* code */
+    break;
+
+  case moving_st:
+    /* code */
+    break;
+
+  case dead_st:
+    /* code */
+    break;
+  }
+}
 
 bool ball_hit_ground() {}
+
+void drawBall() {
+  display_fillCircle(x_current, y_current, BALL_RADIUS, BALL_COLOR);
+}
+
+void eraseBall() {
+  display_fillCircle(x_current, y_current, BALL_RADIUS, BACKGROUND_COLOR);
+}
--- lab9_project/main.c
+++ lab9_project/main.c
@@ -1,17 +1,57 @@
 
-
+#include "display.h"
 #include "gameControl.h"
-
+#include "interrupts.h"
+#include "intervalTimer.h"
+#include "touchscreen.h"
 #include <stdio.h>
 
-int main(){
+#define TOUCHSCREEN_TIMER_PERIOD 10.0E-3
+#define GAME_TIMER_PERIOD 45.0E-3
 
+volatile bool interrupt_flag;
 
-
-
-// Run tick functions
-while(1){
-    //when a tick happens call gamecontrol tick
+// Interrupt handler for game - use flag method so that it can be interrupted by
+// the touchscreen tick while running.
+void game_isr() {
+  intervalTimer_ackInterrupt(INTERVAL_TIMER_0);
+  interrupt_flag = true;
 }
 
+// Interrupt handler for touchscreen - tick directly
+void touchscreen_isr() {
+  intervalTimer_ackInterrupt(INTERVAL_TIMER_1);
+  touchscreen_tick();
+}
+
+int main() {
+
+  display_init();
+  touchscreen_init(TOUCHSCREEN_TIMER_PERIOD);
+  gameControl_init();
+
+  // Initialize timer interrupts
+  interrupts_init();
+  interrupts_register(INTERVAL_TIMER_0_INTERRUPT_IRQ, game_isr);
+  interrupts_register(INTERVAL_TIMER_1_INTERRUPT_IRQ, touchscreen_isr);
+  interrupts_irq_enable(INTERVAL_TIMER_0_INTERRUPT_IRQ);
+  interrupts_irq_enable(INTERVAL_TIMER_1_INTERRUPT_IRQ);
+
+  intervalTimer_initCountDown(INTERVAL_TIMER_0, GAME_TIMER_PERIOD);
+  intervalTimer_initCountDown(INTERVAL_TIMER_1, TOUCHSCREEN_TIMER_PERIOD);
+  intervalTimer_enableInterrupt(INTERVAL_TIMER_0);
+  intervalTimer_enableInterrupt(INTERVAL_TIMER_1);
+  intervalTimer_start(INTERVAL_TIMER_0);
+  intervalTimer_start(INTERVAL_TIMER_1);
+
+  // Run tick functions
+  while (1) {
+    // when a tick happens call gamecontrol tick
+    if(interrupt_flag)
+      {
+        interrupt_flag = false;
+        gameControl_tick();
+      }
+    
+  }
 }
