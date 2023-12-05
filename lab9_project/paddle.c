647c5d0640538c75f3e5adc070af123609c718dd
lancegates <lance.m.gates@gmail.com>
2023-12-04 17:33:01 -0700

    added stuff for the paddle to move

--- lab9_project/paddle.c
+++ lab9_project/paddle.c
@@ -3,15 +3,28 @@
 #include "display.h"
 #include "buttons.h"
 
+//right/left edge of screen 
+#define RIGHT_EDGE 320
+#define LEFT_EDGE 0
+
+//paddle speed
+#define PADDLE_SPEED 4
+
 //middle of screen
 #define START_LOCATION_X 160
-//bottom part of screen
-#define LOCATION_Y 200
 
-#define PADDLE_LENGTH 30
+//bottom part of screen
+#define LOCATION_Y 220
+
+//paddle dimensions
+#define PADDLE_LENGTH 50
 #define PADDLE_WIDTH 5
 
+//paddle current position in x
 uint16_t currentPosition;
+
+//bool for telling if ball hit paddle
+bool isHit;
 
 // Paddle states
 enum paddle_control_t { INITIAL, GO_RIGHT, GO_LEFT, STOP };
@@ -20,7 +33,12 @@ static enum paddle_control_t currentState;
 
 void drawPaddle() {
     display_fillRect(currentPosition-(PADDLE_LENGTH/2), LOCATION_Y,
-     PADDLE_WIDTH, PADDLE_LENGTH, DISPLAY_WHITE);
+     PADDLE_LENGTH, PADDLE_WIDTH, DISPLAY_WHITE);
+}
+
+void erasePaddle() {
+    display_fillRect(currentPosition-(PADDLE_LENGTH/2), LOCATION_Y,
+     PADDLE_LENGTH, PADDLE_WIDTH, DISPLAY_BLACK);
 }
 
 //initialize paddle
@@ -35,19 +53,31 @@ void paddle_tick() {
   switch (currentState) {
   case INITIAL:
     drawPaddle();
+    buttons_init();
     currentState = STOP;
     break;
 
   case STOP:
-    
+    if ((buttons_read() & BUTTONS_BTN0_MASK) == BUTTONS_BTN0_MASK) {
+        currentState = GO_RIGHT;
+        
+    }
+    else if ((buttons_read() & BUTTONS_BTN3_MASK) == BUTTONS_BTN3_MASK) {
+        currentState = GO_LEFT;
+        
+    }
     break;
 
   case GO_RIGHT:
-    
+    if ((buttons_read() & BUTTONS_BTN0_MASK) != BUTTONS_BTN0_MASK) {
+        currentState = STOP;
+    }
     break;
 
   case GO_LEFT:
-    
+    if ((buttons_read() & BUTTONS_BTN3_MASK) != BUTTONS_BTN3_MASK) {
+        currentState = STOP;
+    }
     break;
   }
 
@@ -58,14 +88,25 @@ void paddle_tick() {
     break;
   
   case STOP:
+    
     break;
 
   case GO_RIGHT:
-    
+    //Check if you reached the right edge
+    if (currentPosition + (PADDLE_LENGTH/2) < RIGHT_EDGE) {
+        erasePaddle();
+        currentPosition = currentPosition + PADDLE_SPEED;
+        drawPaddle();
+    }
     break;
 
   case GO_LEFT:
-    
+    //Check if you reached the left edge
+    if (currentPosition - (PADDLE_LENGTH/2) > LEFT_EDGE) {
+        erasePaddle();
+        currentPosition = currentPosition - PADDLE_SPEED;
+        drawPaddle();
+    }
     break;
   }
 }
@@ -77,4 +118,8 @@ void paddle_get_angle();
 bool paddle_is_hit(); 
 
 // Get the XY location of the paddle
-display_point_t paddle_getXY();
+display_point_t paddle_getXY() {
+    display_point_t myPoint;
+    myPoint.x = currentPosition;
+    myPoint.y = LOCATION_Y;
+}
